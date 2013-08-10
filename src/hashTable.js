7b991a63e839f4dbe4ff6ddf5da519a3c7442dba
Hack Reactor Students <github@hackreactor.com>
2013-08-10 16:59:52 -0700

    refactored hashTables.js

--- src/hashTable.js
+++ src/hashTable.js
@@ -1,14 +1,5 @@
 var HashTable = function(){
   this._limit = 8;
-
-  // Use a limited array to store inserted elements.
-  // It'll keep you from using too much space. Usage:
-  //
-  //   limitedArray.set(3, 'hi');
-  //   limitedArray.get(3); // alerts 'hi'
-  //
-  // There's also a '.each' method that you might find
-  // handy once you're working on resizing
   this._storage = makeLimitedArray(this._limit);
 };
 
@@ -23,22 +14,13 @@ HashTable.prototype.insert = function(k, v){
 HashTable.prototype.retrieve = function(k){
   var value;
   var i = getIndexBelowMaxForKey(k, this._limit);
-  var elements = this._storage.get(i); //this is our problem
+  var elements = this._storage.get(i);
   console.log(elements);
   _.each(elements, function(el){
-    if(el[0] === k){
-      value = el[1];
-    }
+    el[0] === k && (value = el[1]);
   });
-
   return value;
-};/*elements = an array of arrays
-not sure about test, need to get value from an array in elements
-*/
+};
 
 HashTable.prototype.remove = function(){
 };
-
-// NOTE: For this code to work, you will NEED the code from hashTableHelpers.js
-// Start by loading those files up and playing with the functions it provides.
-// You don't need to understand how they work, only their interface is important to you
